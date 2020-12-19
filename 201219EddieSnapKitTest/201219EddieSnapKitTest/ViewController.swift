3aa48182b934556205ea0e37df418ecd34dd18fd
안상욱 <>
2020-12-19 16:23:56 +0900

    201219 1st commit

---
+++ 201219EddieSnapKitTest/201219EddieSnapKitTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  201219EddieSnapKitTest
+//
+//  Created by SWAhn on 2020/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let test = "test"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
