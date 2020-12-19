d351acd63952aba72d49cc8407385f302ee610be
안상욱 <>
2020-12-19 16:36:54 +0900

    snapkit size and center box

--- 201219EddieSnapKitTest/201219EddieSnapKitTest/ViewController.swift
+++ 201219EddieSnapKitTest/201219EddieSnapKitTest/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
-    let test = "test"
+    
+    let Abox: UIView = {
+        let box = UIView()
+        box.backgroundColor = .red
+        return box
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(Abox)
+        Abox.snp.makeConstraints { (make) in
+            make.size.equalTo(100)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
         
         // Do any additional setup after loading the view.
     }
